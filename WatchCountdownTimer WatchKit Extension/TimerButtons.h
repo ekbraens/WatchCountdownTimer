957aae6410f075a9ecf3ef9893af4e30eaa66a5b
New <eric.braen@gmail.com>
2014-12-07 19:00:17 -0800

    added storyboard elements

---
+++ WatchCountdownTimer WatchKit Extension/TimerButtons.h
@@ -0,0 +1,13 @@
+//
+//  TimerButtons.h
+//  WatchCountdownTimer
+//
+//  Created by New on 12/7/14.
+//  Copyright (c) 2014 Braen. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface TimerButtons : WKInterfaceController
+
+@end
